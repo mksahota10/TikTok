09d1b0ee89d6cabcac27f344ae456abbb0bd94ef
mksahota10 <mksahota10@gmail.com>
2021-04-10 20:29:22 -0700

    css

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBZ1kCfffeJ4jhod1X7XtuLCTdugdSai4Q",
+    authDomain: "tiktok-4806d.firebaseapp.com",
+    projectId: "tiktok-4806d",
+    storageBucket: "tiktok-4806d.appspot.com",
+    messagingSenderId: "620319074646",
+    appId: "1:620319074646:web:fc50f483d0aa58a6820ef7",
+    measurementId: "G-0LV7HPMX1T"
+  };
